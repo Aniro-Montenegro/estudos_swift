d619413bd5572ca5f126298f6799d4460bb95718
Aniro <aniro.montenegro@flapper.aero>
2023-09-10 21:59:30 -0300

    informacoes pessoais

---
+++ primeiro_programa/Informacoes Pessoais.playground/Contents.swift
@@ -0,0 +1,19 @@
+var nome : String
+var inicial : Character
+var apelido : String
+var idade : Int
+var altura : Double
+var gostaSwift : Bool
+
+inicial = "A"
+nome = "Aniro Montenegro"
+apelido = "Niro"
+idade = 44
+altura = 1.72
+gostaSwift = true
+print("Inicial do nome \(inicial)")
+print("Nome: \(nome)")
+print("Apelido: \(apelido)")
+print("Idade \(idade)")
+print("Altura \(altura)")
+print("Gosta de swift \(gostaSwift)")
