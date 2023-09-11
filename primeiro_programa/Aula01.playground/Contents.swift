9dbb850d68216d20f2986bae70145ff48629af38
Aniro <aniro.montenegro@flapper.aero>
2023-09-10 21:45:46 -0300

    variaveis

---
+++ primeiro_programa/Aula01.playground/Contents.swift
@@ -0,0 +1,19 @@
+
+var nome = "Aniro Montenegro"
+var cidade = "São Josê dos Campos"
+var tempo = 20
+print("Meu nome é \(nome) e moro em \(cidade) e moro aqui há \(tempo) anos")
+
+var carro: String = "Onix"
+var cor : String = "Branco"
+var ano : Int = 2019
+print("Tenho carro \(carro) de cor \(cor) e do ano \(ano)")
+
+
+var nota1 = 7.5
+var nota2 = 8.9
+var total : Double = nota1+nota2
+print("A nota total é \(total)")
+var aprovado : Bool
+var notaConceito : Character = "C"
+aprovado = true
