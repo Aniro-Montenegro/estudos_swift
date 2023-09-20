80aaa08c07a387b3c13a7fd90789f86da08d8924
Aniro <aniro.montenegro@flapper.aero>
2023-09-20 10:37:17 -0300

    operacoes aritmeticas

---
+++ operacoes_artmeticas.playground/Contents.swift
@@ -0,0 +1,23 @@
+10 + 2
+10 - 2
+8 * 2
+10 / 2
+5 / 2
+5.0 / 2
+ 5 % 2
+4 % 2
+5 + 2 * 10
+(5 + 2) * 10
+
+var numero1 = 10
+var numero2 = 50
+var soma = numero1 + numero2
+print(soma)
+numero1 += 2
+print(numero1)
+
+var nome = "Aniro"
+var sobrenome = "Montenegro"
+print(nome+sobrenome)
+var nomeCompleto = "meu nome é : \(nome) \(sobrenome)"
+print(nomeCompleto)
